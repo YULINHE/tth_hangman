fcaaf7c1bd6394b69fe6106f9ee5fd4acdb0381d
YULINHE <yulinhe@yahoo.com>
2017-03-22 10:45:52 +0800

    stage 3

--- ex1/Hangman.java
+++ ex1/Hangman.java
@@ -2,7 +2,18 @@ public class Hangman {
 
 	public static void main(String[] args) {
 
-		Game game = new Game("Treehouse");
+		Game game = new Game("treehouse");
+		Prompter prompter = new Prompter(game);
+		prompter.displayProgress();
+		boolean isHit = prompter.promterForGuess();
+		if (isHit) {
+			System.out.println("We got a hit!");
+			
+		} else {
+			System.out.println("Oops missed£¡")£»
+		}
+		prompter.displayProgress();
 	}
+	
 
 }
--- ex1/Prompter.java
+++ ex1/Prompter.java
@@ -1,3 +1,21 @@
-public class Prompter {
+import java.util.Scanner;
 
+public class Prompter {
+	private Game game;
+	
+	public Prompter(Game game) {
+		this.game = game;
+	}
+	public boolean promptForGuess() {
+		Scanner scanner = new Scanner(System.in);
+		System,.out.print("Enter a letter: ");
+		String guessInput = scanner.nextLine();
+		char guess = guessInput.codeAt(0);
+		return game.applyGuess(guess);
+	}
+	public void displayProgress(){
+		System.out.printf("Try ro solve:  %s%n", 
+						  game.getCurrentProgress());
+		
+	}
 }
